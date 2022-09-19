1ba5b5549193063e3233f16e8664f56ed3d82c6a
Anatoliy K <778966@mail.ru>
2022-09-19 14:08:11 +0500

    added method array

---
+++ Example04_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 21, 82, 23, 68, 34, 65, 68, 57, 18 };
+
+int n = array.Length;
+int find = 68;
+int index = 0;
+while(index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
