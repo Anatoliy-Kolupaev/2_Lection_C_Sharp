5da2b5174c4691f99b097de4c635b26d7fc6cb03
Anatoliy K <778966@mail.ru>
2022-09-19 13:54:41 +0500

    added array

---
+++ Example02_array/Program.cs
@@ -0,0 +1,15 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;  
+    return result;  
+}
+//              0   1   2   3   4   5   6   7   8   - это индексы элементов в массиве
+int[] array = {21, 42, 831, 64, 35, 46, 87, 58, 19}; 
+int max = Max (
+    Max (array [0], array [1], array[2]),
+    Max (array [3], array [4], array[5]),
+    Max (array [5], array [6], array[8]));
+
+Console.WriteLine(max);
